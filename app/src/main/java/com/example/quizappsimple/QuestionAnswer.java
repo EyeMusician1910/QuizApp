d27784f286b699c9d7714e7ee8f3a811f6c1a0a8
EyeMusician1910 <dp19102004d@gmail.com>
2025-06-08 11:23:30 +0530

    First Commit

---
+++ app/src/main/java/com/example/quizappsimple/QuestionAnswer.java
@@ -0,0 +1,41 @@
+package com.example.quizappsimple;
+
+public class QuestionAnswer {
+    public static String question[] = {
+            "What is the largest planet in the solar system?",
+            "Who is the founder of Microsoft?",
+            "Which language is used for Android app development?",
+            "What is the capital of Japan?",
+            "Which company created the iPhone?",
+            "What does CPU stand for?",
+            "Which one is a search engine?",
+            "Which country is famous for pizza?",
+            "What is the chemical symbol for water?",
+            "Who developed the theory of relativity?"
+    };
+    public static String choices[][] = {
+            {"Earth", "Mars", "Jupiter", "Venus"},
+            {"Bill Gates", "Steve Jobs", "Mark Zuckerberg", "Elon Musk"},
+            {"Java", "Python", "HTML", "PHP"},
+            {"Seoul", "Tokyo", "Beijing", "Bangkok"},
+            {"Google", "Apple", "Samsung", "Sony"},
+            {"Central Process Unit", "Central Processing Unit", "Central Power Unit", "Central Program Unit"},
+            {"Bing", "Instagram", "Facebook", "WhatsApp"},
+            {"France", "Italy", "USA", "India"},
+            {"H2O", "CO2", "O2", "NaCl"},
+            {"Isaac Newton", "Albert Einstein", "Galileo Galilei", "Nikola Tesla"}
+    };
+
+    public static String correctAnswers[] = {
+            "Jupiter",
+            "Bill Gates",
+            "Java",
+            "Tokyo",
+            "Apple",
+            "Central Processing Unit",
+            "Bing",
+            "Italy",
+            "H2O",
+            "Albert Einstein"
+    };
+}
